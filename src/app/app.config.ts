8db010ea2720562844c9720d9a96d4ef27f79f88
gourav-soni-2024 <gourav.soni@systango.com>
2025-01-23 20:31:18 +0530

    call zipcode api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,14 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     providePrimeNG({
       theme: {
           preset: Aura,
--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import intlTelInput, { Iti } from 'intl-tel-input';
 import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
 import { ButtonModule } from 'primeng/button';
@@ -9,6 +9,8 @@ import { NgxPhoneField } from 'ngx-phone-field';
 import { DatePickerModule } from 'primeng/datepicker';
 import { InputTextModule } from 'primeng/inputtext';
 import { SelectModule } from 'primeng/select';
+import { HttpClient } from '@angular/common/http';
+import { postcodeValidator } from 'postcode-validator';
 
 
 @Component({
@@ -27,168 +29,60 @@ import { SelectModule } from 'primeng/select';
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
   countries = intlTelInput.getCountryData();
-  filteredCountries: any[] = [];
   filteredAddresses: any[] = [];
   params = {
     initialCountry: 'us',
     countryOrder: ["us","gb","in"],
     allowDropdown: true,
     formatAsYouType: true,
-    nationalMode:false,
     // @ts-ignore
     // load utils script for formatting and validation
     loadUtilsOnInit: async () => import('intl-tel-input/utils'), 
   };
-  phone: FormControl = new FormControl<Iti | null>(null);
-  selectedCountry: FormControl = new FormControl("");
-  selectedAddress: FormControl = new FormControl("");
+  phone: FormControl = new FormControl<Iti | string>("", [Validators.required]);
+  selectedCountry: FormControl = new FormControl("", Validators.required);
+  selectedAddress: FormControl = new FormControl("", [this.validate()]);
 
-  ngOnInit(): void {
-    console.log("Countries", this.countries);
-  }
-
-  filterCountry(event: AutoCompleteCompleteEvent) {
-    let filtered: any[] = [];
-    let query = event.query;
-
-    for (let i = 0; i < (this.countries as any[]).length; i++) {
-      let country = (this.countries as any[])[i];
-      if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-        filtered.push(country);
-      }
-    }
-
-    this.filteredCountries = filtered;
-  }
+  constructor(private http: HttpClient) {}
 
   filterAddress(event: AutoCompleteCompleteEvent) {
-    // https://app.zipcodebase.com/api/v1/search?apikey=YOUR-APIKEY&codes=10005%2C51503
-    const data: any = {
-      "results": {
-        "73001": [
-          {
-            "postal_code": "73001",
-            "country_code": "US",
-            "latitude": "35.20320000",
-            "longitude": "-98.35740000",
-            "city": "Albert",
-            "state": "Oklahoma",
-            "city_en": "Albert",
-            "state_en": "Oklahoma",
-            "state_code": "OK",
-            "province": "Caddo",
-            "province_code": "015"
-        }],
-      "452001": [
-          {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Radio Colony Indore",
-          "state": "Madhya Pradesh",
-          "city_en": "Radio Colony Indore",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
-        },
-        {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Indore G.P.O.",
-          "state": "Madhya Pradesh",
-          "city_en": "Indore G.P.O.",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
-        },
-        {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Indore Uchchanyayalay",
-          "state": "Madhya Pradesh",
-          "city_en": "Indore Uchchanyayalay",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
-        },
-        {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Indore CGO Complex",
-          "state": "Madhya Pradesh",
-          "city_en": "Indore CGO Complex",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
-        },
-        {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Indore Takshashila",
-          "state": "Madhya Pradesh",
-          "city_en": "Indore Takshashila",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
-        },
-        {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Indore Manorama Ganj",
-          "state": "Madhya Pradesh",
-          "city_en": "Indore Manorama Ganj",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
-        },
-        {
-          "postal_code": "452001",
-          "country_code": "IN",
-          "latitude": "22.72330000",
-          "longitude": "75.87000000",
-          "city": "Indore Tukoganj",
-          "state": "Madhya Pradesh",
-          "city_en": "Indore Tukoganj",
-          "state_en": "Madhya Pradesh",
-          "state_code": "MP",
-          "province": "Indore",
-          "province_code": "IN"
+    const country = this.phone.value['defaultCountry'];
+    if (this.selectedAddress.valid && country) {
+      console.log("country", country);
+      const postalCode = this.selectedAddress.value;
+      const url = `https://app.zipcodebase.com/landing_demo/?codes=${postalCode}&country=${country}`;
+      const responseData = this.http.get(url);
+      responseData.subscribe({
+        next: ((data: any)=> {
+      console.log("data", data);
+        let filtered: any[] = [];
+        const addresses = data.results[this.selectedAddress.value];
+        if(data.results[this.selectedAddress.value]) {
+        for (let i = 0; i < addresses.length; i++) {
+            let address = addresses[i];
+            filtered.push(address.postal_code + ' - ' + address.city_en + ', ' + address.state_code + ', ' + address.country_code);
+          }
         }
-        ]
-      }
-    };
-
-    let filtered: any[] = [];
-    let query = event.query;
-    const addresses = data.results[this.selectedAddress.value];
-    console.log("addresses", addresses);
-    if(data.results[this.selectedAddress.value]) {
-    for (let i = 0; i < addresses.length; i++) {
-        let address = addresses[i];
-        console.log("address", address, address.city_en.toLowerCase().match(query.toLowerCase()))
-        filtered.push(address.city_en + ' ' + address.state_code + ' ' + address.country_code);
-      }
+        console.log("filteredAddresses", filtered);
+        this.filteredAddresses = filtered;
+      }),
+      error: ((error) =>{
+        console.log("error", error)
+      })});
     }
-    this.filteredAddresses = filtered;
+  }
+
+  private validate(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+      if (!control) return null;
+      const country = this.phone?.value?.['defaultCountry'] ? this.phone?.value?.['defaultCountry'].toUpperCase() : null;
+      if (!country) return { isInvalidAddress: true };
+      const address = this.selectedAddress?.value
+      if (!address) { isInvalidAddress: true };
+      return !postcodeValidator(control?.value, country) ? {isInvalidAddress: true} : null;
+    }
   }
 
 }
