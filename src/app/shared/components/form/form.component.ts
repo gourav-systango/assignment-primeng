442f72424837a8e2d37b33569bc3732deaf8ed04
gourav-soni-2024 <gourav.soni@systango.com>
2025-01-23 17:22:23 +0530

    get address with zipcode

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -36,6 +36,7 @@ export class FormComponent implements OnInit {
     countryOrder: ["us","gb","in"],
     allowDropdown: true,
     formatAsYouType: true,
+    nationalMode:false,
     // @ts-ignore
     // load utils script for formatting and validation
     loadUtilsOnInit: async () => import('intl-tel-input/utils'), 
@@ -63,44 +64,131 @@ export class FormComponent implements OnInit {
   }
 
   filterAddress(event: AutoCompleteCompleteEvent) {
-    const addresses: any[] = [{
-      "postal_code": "CR2 6XH",
-      "country_code": "GB",
-      "latitude": "51.34670000",
-      "longitude": "-0.10340000",
-      "city": "London",
-      "state": "England",
-      "city_en": "London",
-      "state_en": "England",
-      "state_code": "ENG",
-      "province": "Greater London",
-      "province_code": "GLA"
-    },
-    {
-      "postal_code": "452001",
-      "country_code": "IN",
-      "latitude": "22.72330000",
-      "longitude": "75.87000000",
-      "city": "Radio Colony Indore",
-      "state": "Madhya Pradesh",
-      "city_en": "Radio Colony Indore",
-      "state_en": "Madhya Pradesh",
-      "state_code": "MP",
-      "province": "Indore",
-      "province_code": "IN"
-    }];
+    // https://app.zipcodebase.com/api/v1/search?apikey=YOUR-APIKEY&codes=10005%2C51503
+    const data: any = {
+      "results": {
+        "73001": [
+          {
+            "postal_code": "73001",
+            "country_code": "US",
+            "latitude": "35.20320000",
+            "longitude": "-98.35740000",
+            "city": "Albert",
+            "state": "Oklahoma",
+            "city_en": "Albert",
+            "state_en": "Oklahoma",
+            "state_code": "OK",
+            "province": "Caddo",
+            "province_code": "015"
+        }],
+      "452001": [
+          {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Radio Colony Indore",
+          "state": "Madhya Pradesh",
+          "city_en": "Radio Colony Indore",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        },
+        {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Indore G.P.O.",
+          "state": "Madhya Pradesh",
+          "city_en": "Indore G.P.O.",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        },
+        {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Indore Uchchanyayalay",
+          "state": "Madhya Pradesh",
+          "city_en": "Indore Uchchanyayalay",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        },
+        {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Indore CGO Complex",
+          "state": "Madhya Pradesh",
+          "city_en": "Indore CGO Complex",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        },
+        {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Indore Takshashila",
+          "state": "Madhya Pradesh",
+          "city_en": "Indore Takshashila",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        },
+        {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Indore Manorama Ganj",
+          "state": "Madhya Pradesh",
+          "city_en": "Indore Manorama Ganj",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        },
+        {
+          "postal_code": "452001",
+          "country_code": "IN",
+          "latitude": "22.72330000",
+          "longitude": "75.87000000",
+          "city": "Indore Tukoganj",
+          "state": "Madhya Pradesh",
+          "city_en": "Indore Tukoganj",
+          "state_en": "Madhya Pradesh",
+          "state_code": "MP",
+          "province": "Indore",
+          "province_code": "IN"
+        }
+        ]
+      }
+    };
 
     let filtered: any[] = [];
     let query = event.query;
-
+    const addresses = data.results[this.selectedAddress.value];
+    console.log("addresses", addresses);
+    if(data.results[this.selectedAddress.value]) {
     for (let i = 0; i < addresses.length; i++) {
-      let address = addresses[i];
-      console.log("address", address, address.city_en.toLowerCase().match(query.toLowerCase()))
-      if (address.city_en.toLowerCase().match(query.toLowerCase())) {
-        filtered.push(address.city_en + ' ' + address.state_code + ' ' + address.province_code);
+        let address = addresses[i];
+        console.log("address", address, address.city_en.toLowerCase().match(query.toLowerCase()))
+        filtered.push(address.city_en + ' ' + address.state_code + ' ' + address.country_code);
       }
     }
-
     this.filteredAddresses = filtered;
   }
+
 }
