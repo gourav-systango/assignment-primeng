b61d3ec1488f394fb81ea1dbc1ddd7459832707d
gourav-soni-2024 <gourav.soni@systango.com>
2025-01-29 19:31:26 +0530

    get event on country change, and set pre value on tel-input

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, viewChild } from '@angular/core';
 import { AbstractControl, FormControl, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import intlTelInput, { Iti } from 'intl-tel-input';
 import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
@@ -28,10 +28,11 @@ import { postcodeValidator } from 'postcode-validator';
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
-  countries = intlTelInput.getCountryData();
-  filteredAddresses: any[] = [];
-  params = {
+export class FormComponent implements AfterViewInit {
+  // @viewChild("Contact") contact
+  public countries = intlTelInput.getCountryData();
+  public filteredAddresses: any[] = [];
+  public params = {
     initialCountry: 'us',
     countryOrder: ["us","gb","in"],
     allowDropdown: true,
@@ -47,20 +48,30 @@ export class FormComponent {
     // load utils script for formatting and validation
     loadUtilsOnInit: async () => import('intl-tel-input/utils'), 
   };
-  postalCode: string = "";
-  phone: FormControl = new FormControl<Iti | string>("", [Validators.required,this.validateNumber()]);
-  selectedCountry: FormControl = new FormControl("", Validators.required);
-  selectedAddress: FormControl = new FormControl("", [this.validate()]);
+  public postalCode: string = "";
+  public phone: FormControl = new FormControl<Iti | string>("", [Validators.required,this.validateNumber()]);
+  public selectedCountry: FormControl = new FormControl("", Validators.required);
+  public selectedAddress: FormControl = new FormControl("", [this.validate()]);
 
   constructor(private http: HttpClient) {}
 
-  filterAddress(event: AutoCompleteCompleteEvent) {
-    /* 
-      const iti: any  = intlTelInput.utils;
-      const number = telInput.getNumber(iti?.numberFormat.E164); // returns tel input value in simple number format with dial code
-      iti.formatNumber(number, telInput?.defaultCountry) // returns tel input value with dial code
-      iti.formatNumberAsYouType(number, telInput?.defaultCountry) // returns tel input value as iso formatd with dial code
-    */
+  public ngAfterViewInit(): void {
+    const input: any = document.querySelector("#phone");
+    setTimeout(() => {
+      const telInstance = intlTelInput.getInstance(input); // get input Instance  
+      telInstance?.setCountry("in") // set country
+      telInstance?.setNumber("+919826098260") // set number
+    }, 1000);
+
+    input.addEventListener("countrychange", () => { // event receive on country change
+      if(this.phone.value['defaultCountry']) {
+        const telInstance = intlTelInput.getInstance(input);
+        telInstance?.setNumber("") // set input value in null on every event if country change
+      }
+    });
+  }
+
+  public filterAddress(event: AutoCompleteCompleteEvent): void {
     const country = this.phone.value['defaultCountry'];
     if (this.selectedAddress.valid && country) {
       const postalCode = this.selectedAddress.value;
@@ -87,7 +98,26 @@ export class FormComponent {
     }
   }
 
-  validateNumber(): ValidatorFn {
+  public onSubmit(): void {
+    const iti: any  = intlTelInput.utils;
+    const telInput =  this.phone.value;
+    
+    const number = telInput.getNumber(iti?.numberFormat.E164); // returns tel input value in simple number format with dial code
+    iti.formatNumber(number, telInput?.defaultCountry) // returns tel input value with dial code
+    iti.formatNumberAsYouType(number, telInput?.defaultCountry) // returns tel input value as iso formatd with dial code
+    
+    const telInputObject = {
+      selectedCountryData: telInput.selectedCountryData,
+      // dialCode: telInput.getExtension(),
+      number: telInput.getNumber(iti?.numberFormat.E164),
+      formatNumber: iti.formatNumber(number, telInput?.defaultCountry),
+      formatNumberAsYouType: iti.formatNumberAsYouType(number, telInput?.defaultCountry),
+      defaultInput: telInput
+    }
+    console.log("telInputObject", telInputObject);
+  }
+
+  private validateNumber(): ValidatorFn {
     return (control:AbstractControl) : ValidationErrors | null => {
       if (!control) return null;
       const telInput: any = control?.value;
